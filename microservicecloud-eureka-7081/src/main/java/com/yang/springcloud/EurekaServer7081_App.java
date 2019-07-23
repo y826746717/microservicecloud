4bd4afb863ca22536ae1b27bd2561fbc200c26bf
yangyang <y826746717@126.com>
2019-07-23 23:02:21 +0800

    添加eureka服务注册与发现功能，明天写eureka集群

---
+++ microservicecloud-eureka-7081/src/main/java/com/yang/springcloud/EurekaServer7081_App.java
@@ -0,0 +1,15 @@
+package com.yang.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer    //EurekaServer服务器端启动类,接受其它微服务注册进来
+//本服务启动后，会自动注册进 eureka 服务中
+//eureka的服务端
+public class EurekaServer7081_App {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer7081_App.class,args);
+    }
+}
